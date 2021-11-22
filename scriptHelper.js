caa1466545f1644ce256710f9211a7fdb9a7d704
Kat <kathryn.garnier@gmail.com>
2021-11-22 18:14:14 -0500

    Fetched planetary data

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,19 +2,19 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
+   const div = document.getElementById("missionTarget")
+   div.innerHTML = `
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
                 </ol>
-                <img src="">
-   */
-}
+                <img src="${imageUrl}">
+            `
+};
 
 function validateInput(testInput) {
    if (testInput === "") {
@@ -24,7 +24,7 @@ function validateInput(testInput) {
    } else {
        return "Is a Number"
    }
-}
+};
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass) {
     const formItems = [pilot, copilot, fuelLevel, cargoMass]
@@ -61,19 +61,19 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass) {
 };
 
 async function myFetch() {
-    let planetsReturned;
+    let planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json");
+    let returnedPlanets = await planetsReturned.json();
 
-    planetsReturned = await fetch().then( function(response) {
-        });
-
-    return planetsReturned;
-}
+    return returnedPlanets;
+};
 
 function pickPlanet(planets) {
-}
+    return Math.floor(Math.random() * planets.length)
+};
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
 module.exports.pickPlanet = pickPlanet; 
 module.exports.myFetch = myFetch;
+
