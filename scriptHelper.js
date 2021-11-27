a6825d9fa9f65d60160cead8da7c3158ab64a2a1
Kat <kathryn.garnier@gmail.com>
2021-11-26 22:19:58 -0500

    fixing dom manipulation 4

--- scriptHelper.js
+++ scriptHelper.js
@@ -4,16 +4,16 @@ require('isomorphic-fetch');
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    const div = document.getElementById("missionTarget")
    div.innerHTML = `
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: ${name}</li>
-                    <li>Diameter: ${diameter}</li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: ${distance}</li>
-                    <li>Number of Moons: ${moons}</li>
-                </ol>
-                <img src="${imageUrl}">
-            `
+        <h2>Mission Destination</h2>
+            <ol>
+                <li>Name: ${name}</li>
+                <li>Diameter: ${diameter}</li>
+                <li>Star: ${star}</li>
+                <li>Distance from Earth: ${distance}</li>
+                <li>Number of Moons: ${moons}</li>
+            </ol>
+        <img src="${imageUrl}">
+    `
 };
 
 function validateInput(testInput) {
@@ -53,15 +53,13 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass) {
             launchNotReady();
             if (fuelLevel < 10000) {
                 fuelStatus.innerHTML = "Fuel level too low for launch";
-            } else {
-                fuelStatus.innerHTML = "Fuel level high enough for launch";
             }
             if (cargoMass > 10000) {
                 cargoStatus.innerHTML = "Cargo mass too heavy for launch";
-            } else {
-                cargoStatus.innerHTML = "Cargo mass low enough for launch";
             }
         } else {
+            fuelStatus.innerHTML = "Fuel level high enough for launch";
+            cargoStatus.innerHTML = "Cargo mass low enough for launch";
             launchStatus.innerHTML = "Shuttle is Ready for Launch";
             launchStatus.style.color = "rgb(65, 159, 106)";
         }
@@ -86,4 +84,3 @@ module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
 module.exports.pickPlanet = pickPlanet; 
 module.exports.myFetch = myFetch;
-
